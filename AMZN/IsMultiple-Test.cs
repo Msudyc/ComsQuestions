45035960063bfc1b7196c6523e7272e0573b3594
Msudyc <msudyc@gmail.com>
2015-01-06 22:46:06 -0800

    Copy files over from swintv

---
+++ AMZN/IsMultiple-Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AMZN
+{
+    [TestClass]
+    public partial class AMZN
+    {
+        [TestMethod]
+        public void AMZN_IsMultipleTest1()
+        {
+            Assert.IsTrue(IsMultiple("abcabcabc"));
+            Assert.IsFalse(IsMultiple("bcdbcdbcde"));
+            Assert.IsTrue(IsMultiple("abcdabcd"));
+            Assert.IsFalse(IsMultiple("xyz"));
+            Assert.IsFalse(IsMultiple("aaaaaaaaaa"));
+        }
+    }
+}
